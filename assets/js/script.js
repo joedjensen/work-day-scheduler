f943d92d9c75417d300d53fae29a8f17cad2a998
josephjensen <joe@cryptonumerics.com>
2022-10-06 09:56:34 -0700

    add support for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,41 @@ var currentDateEl = $("#currentDay")
 var calendarEl = $("#calendar")
 currentDateEl.text(dayjs().format("dddd, MMMM D"));
 var currentHour = dayjs().hour();
-
-var hourObjectArray =[
-    {
-        "name" : "9am",
-    },
-    {
-        "name" : "10am",
-    },
-    {
-        "name" : "11am",
-    },
-    {
-        "name" : "12pm",
-    },
-    {
-        "name" : "1pm",
-    },
-    {
-        "name" : "2pm",
-    },
-    {
-        "name" : "3pm",
-    },
-    {
-        "name" : "4pm",
-    },
-    {
-        "name" : "5pm",
-    }
-]
+var hourObjectArray;
+if (localStorage.getItem("hourObjectArray")) {
+    hourObjectArray = JSON.parse(localStorage.getItem("hourObjectArray"))
+} else {
+     hourObjectArray =[
+        {
+            "name" : "9am",
+            "eventText": "code"
+        },
+        {
+            "name" : "10am",
+        },
+        {
+            "name" : "11am",
+        },
+        {
+            "name" : "12pm",
+        },
+        {
+            "name" : "1pm",
+        },
+        {
+            "name" : "2pm",
+        },
+        {
+            "name" : "3pm",
+        },
+        {
+            "name" : "4pm",
+        },
+        {
+            "name" : "5pm",
+        }
+    ]
+}
 
 for (i=0; i < hourObjectArray.length; i++){
     rowEl = $('<div>' , {"class":"row"})
@@ -47,10 +52,21 @@ for (i=0; i < hourObjectArray.length; i++){
         entryBlockEl.addClass("future")
     }
     buttonEl.append(buttonImageEl)
+    buttonEl.attr("data-index",i)
     hourEl.text(hourObjectArray[i].name)
+    if (hourObjectArray[i].eventText) {
+        entryBlockEl.text(hourObjectArray[i].eventText)
+    }
     console.log(entryBlockEl)
     rowEl.append(hourEl)
     rowEl.append(entryBlockEl)
     rowEl.append(buttonEl)
     calendarEl.append(rowEl)
+}
+
+$("button").on("click", saveScore)
+
+function saveScore(event) {
+    hourObjectArray[$(this).attr("data-index")]['eventText'] = $(this).prev().val()
+    localStorage.setItem("hourObjectArray" , JSON.stringify(hourObjectArray))
 }
